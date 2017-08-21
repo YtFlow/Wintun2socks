73c152c190ef2fabd9fd1e6eeee9f5149133a559
bdbai <bdbaiapp@163.com>
2017-08-22 00:50:56 +0800

    Initial commit

---
+++ src/Wintun.cpp
@@ -0,0 +1,49 @@
+﻿#include "Wintun.h"
+#include <string>
+
+namespace Wintun2socks {
+	Wintun^ Wintun::m_instance = ref new Wintun();
+	netif* Wintun::m_interface = netif_default;
+	tcp_pcb* Wintun::m_listenPCB;
+	err_t(__stdcall Wintun::outputPCB) (struct netif *netif, struct pbuf *p,
+		const ip4_addr_t *ipaddr) {
+		auto arr = ref new Platform::Array<uint8, 1u>(p->tot_len);
+		pbuf_copy_partial(p, arr->begin(), p->tot_len, 0);
+		m_instance->PacketPoped(m_instance, arr);
+		return ERR_OK;
+	}
+
+	void Wintun::Init() {
+		lwip_init();
+
+		// add a listening pcb
+
+		auto pcb = tcp_new();
+		auto addr = ip_addr_any;
+		tcp_bind(pcb, &addr, 0);
+		pcb = tcp_listen_with_backlog(pcb, (UINT)TCP_DEFAULT_LISTEN_BACKLOG);
+		Wintun::m_listenPCB = pcb;
+		tcp_accept(pcb, (tcp_accept_fn)&TcpSocket::tcpAcceptFn);
+		m_interface = netif_list;
+		m_interface->mtu = 1500;
+		m_interface->output = (netif_output_fn)&Wintun::outputPCB;
+	}
+
+	Wintun^ Wintun::Instance::get() {
+		return Wintun::m_instance;
+	}
+	uint8 Wintun::PushPacket(const Platform::Array<uint8, 1u>^ packet) {
+		pbuf* p = pbuf_alloc(PBUF_RAW, 0, PBUF_RAM);
+		if (p == NULL) {
+			// Drop it
+			return 1;
+		}
+		p->payload = packet->Data;
+		p->len = p->tot_len = packet->Length;
+		auto iphdr = (const struct ip_hdr *)p->payload;
+		auto proto = iphdr->_proto;
+		if (proto == IP_PROTO_TCP)
+			m_interface->input(p, m_interface);
+		return 0;
+	}
+}
---
+++ src/Wintun.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "lwip\init.h"
+#include "lwip\tcp.h"
+#include "pch.h"
+#include "TcpSocket.h"
+
+namespace WFM = Windows::Foundation::Metadata;
+
+namespace Wintun2socks {
+	public delegate void PacketPopedHandler(Platform::Object^ sender, const Platform::Array<uint8, 1>^ e);
+	ref class Wintun;
+	public interface class IWintun {
+		event PacketPopedHandler^ PacketPoped;
+		void Init();
+		uint8 PushPacket(const Platform::Array<uint8, 1u>^ packet);
+	};
+	public ref class Wintun sealed: [WFM::DefaultAttribute] IWintun
+	{
+	private:
+		static Wintun^ m_instance;
+		static netif* m_interface;
+		static tcp_pcb* m_listenPCB;
+		static err_t(__stdcall Wintun::outputPCB) (struct netif *netif, struct pbuf *p,
+			const ip4_addr_t *ipaddr);
+
+	public:
+		static property Wintun^ Instance { Wintun^ get(); };
+		virtual void Init();
+		virtual uint8 PushPacket(const Platform::Array<uint8, 1u>^ packet);
+		virtual event PacketPopedHandler^ PacketPoped;
+	};
+}
